b24a3f6c9de07761ceb2d76af1aad4d38c605340
Aliaksandr Ptsitsa <ptenix@gmail.com>
2023-03-27 04:51:14 +0300

    feat:fix function showRivalryByRaces

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -24,10 +24,62 @@ export default{
      elf:4,
      undead:8,
      rnd:0,
+     player1score:0,
+     player2score:0,
+
      scoreHO1:0,
      scoreHO2:0,
-     raceArray:[],
-     
+     scoreHH1:0,
+     scoreHH2:0,
+     scoreHU1:0,
+     scoreHU2:0,
+     scoreHE1:0,
+     scoreHE2:0,
+     scoreHR1:0,
+     scoreHR2:0,
+     scoreOH1:0,
+     scoreOH2:0,
+     scoreOO1:0,
+     scoreOO2:0,
+     scoreOU1:0,
+     scoreOU2:0,
+     scoreOE1:0,
+     scoreOE2:0,
+     scoreOR1:0,
+     scoreOR2:0,
+     scoreEO1:0,
+     scoreEO2:0,
+     scoreEH1:0,
+     scoreEH2:0,
+     scoreEU1:0,
+     scoreEU2:0,
+     scoreEE1:0,
+     scoreEE2:0,
+     scoreER1:0,
+     scoreER2:0,
+     scoreUO1:0,
+     scoreUO2:0,
+     scoreUH1:0,
+     scoreUH2:0,
+     scoreUE1:0,
+     scoreUE2:0,
+     scoreUU1:0,
+     scoreUU2:0,
+     scoreUR1:0,
+     scoreUR2:0,
+     scoreRO1:0,
+     scoreRO2:0,
+     scoreRH1:0,
+     scoreRH2:0,
+     scoreRU1:0,
+     scoreRU2:0,
+     scoreRE1:0,
+     scoreRE2:0,
+     scoreRR1:0,
+     scoreRR2:0,
+
+     raceArray:[1],
+     raceArray1:[1],
   }
 
 },   
@@ -68,7 +120,7 @@ this.urlGames1 =`https://website-backend.w3champions.com/api/matches/search?play
 const resScore  = await fetch(this.urlGames1);
 const dataScore  = await resScore.json();
 
-  console.log (dataScore);
+  /* console.log (dataScore); */
   for (let i = 0; i < dataScore.matches.length; i++) {
     if (
       dataScore.matches[i].teams[0].players[0].battleTag == `${this.player1.tag.trim()}` &&
@@ -113,16 +165,19 @@ this.score2=0;
 },
 
 
-async showRivalryByRaces (race1:number,race2:number) {
+async showRivalryByRaces (race1:number,race2:number,var1:any, var2:any) {
    
+let player1score=0;
+let player2score=0;
    for(let i=2; i<15; i++) {
   
  this.urlGames1 =`https://website-backend.w3champions.com/api/matches/search?playerId=${this.player1.tag.trim().replace('#', '%23')}&gateway=20&offset=0&opponentId=${this.player2.tag.trim().replace('#', '%23')}&pageSize=50&season=${i}&gamemode=1`;
  
  const resScore  = await fetch(this.urlGames1);
  const dataScore  = await resScore.json();
- let raceArray:any;
-   console.log (dataScore);
+
+
+
    for (let i = 0; i < dataScore.matches.length; i++) {
      if (
        dataScore.matches[i].teams[0].players[0].battleTag == `${this.player1.tag.trim()}` &&
@@ -131,7 +186,7 @@ async showRivalryByRaces (race1:number,race2:number) {
        dataScore.matches[i].teams[1].players[0].race == `${race2}`
 
      ) {
-       this.scoreHO1++;
+      player1score++;
      }
        else if (
        dataScore.matches[i].teams[0].players[0].battleTag == `${this.player1.tag.trim()}` &&
@@ -140,7 +195,7 @@ async showRivalryByRaces (race1:number,race2:number) {
        dataScore.matches[i].teams[1].players[0].race == `${race2}`
 
        ){
-         this.scoreHO2++;
+        player2score++;
        }
    
      if (
@@ -149,59 +204,63 @@ async showRivalryByRaces (race1:number,race2:number) {
        dataScore.matches[i].teams[0].players[0].race == `${race2}` &&
        dataScore.matches[i].teams[1].players[0].race == `${race1}`
      ) {
-       this.scoreHO1++;
+      player1score++;
      }
      else if (dataScore.matches[i].teams[1].players[0].battleTag == `${this.player1.tag.trim()}` &&
        dataScore.matches[i].teams[1].won == false &&
        dataScore.matches[i].teams[0].players[0].race == `${race2}` &&
        dataScore.matches[i].teams[1].players[0].race == `${race1}`){
-         this.scoreHO2++;
+        player2score++;
+        
        }
 
       
-   /*     raceArray.push(this.scoreHO1);
-       raceArray.push(this.scoreHO2); */
    }
-   console.log(this.scoreHO1,this.scoreHO2);
-  /*  console.log(this.scoreHO1, this.scoreHO2); */
 
 
-   this.raceArray=raceArray;
- 
+
+
+
+  /* console.log(this.scoreOH1,this.scoreOH2,this.scoreOO1,this.scoreOO2,this.scoreOE1,this.scoreOE2,this.scoreOU1,this.scoreOU2,this.scoreOR1,this.scoreOR2,this.scoreUR1, this.scoreUR2,this.scoreUU1, this.scoreUU2,this.scoreUE1, this.scoreUE2, this.scoreUO1, this.scoreUO2,this.scoreUH1, this.scoreUH2 ) */
+  /*  this.raceArray=raceArray; */
+
  }
-
+this.raceArray.shift;
+ this.raceArray.push(player1score,player2score);
+ console.log(this.raceArray)
 
 
 
 },
 showAll (){
-  this.showRivalryByRaces (this.orc,this.human)
-  this.showRivalryByRaces (this.orc,this.orc)
-  this.showRivalryByRaces (this.orc,this.elf)
-  this.showRivalryByRaces (this.orc,this.undead)
-  this.showRivalryByRaces (this.orc,this.rnd)
-  this.showRivalryByRaces (this.human,this.human)
-  this.showRivalryByRaces (this.human,this.orc)
-  this.showRivalryByRaces (this.human,this.elf)
-  this.showRivalryByRaces (this.human,this.undead)
-  this.showRivalryByRaces (this.human,this.rnd)
-  this.showRivalryByRaces (this.elf,this.human)
-  this.showRivalryByRaces (this.elf,this.orc)
-  this.showRivalryByRaces (this.elf,this.elf)
-  this.showRivalryByRaces (this.elf,this.undead)
-  this.showRivalryByRaces (this.elf,this.rnd)
-  this.showRivalryByRaces (this.undead,this.human)
-  this.showRivalryByRaces (this.undead,this.orc)
-  this.showRivalryByRaces (this.undead,this.elf)
-  this.showRivalryByRaces (this.undead,this.undead)
-  this.showRivalryByRaces (this.undead,this.rnd)
-  this.showRivalryByRaces (this.rnd,this.human)
-  this.showRivalryByRaces (this.rnd,this.orc)
-  this.showRivalryByRaces (this.rnd,this.elf)
-  this.showRivalryByRaces (this.rnd,this.undead)
-  this.showRivalryByRaces (this.rnd,this.rnd)
-
-  console.log(this.raceArray)
+  this.showRivalryByRaces (this.orc,this.human, this.scoreOH1,this.scoreOH2)
+  this.showRivalryByRaces (this.orc,this.orc,this.scoreOO1,this.scoreOO2)
+  this.showRivalryByRaces (this.orc,this.elf,this.scoreOE1,this.scoreOE2)
+  this.showRivalryByRaces (this.orc,this.undead, this.scoreOU1,this.scoreOU2)
+  this.showRivalryByRaces (this.orc,this.rnd,this.scoreOR1,this.scoreOR2 )
+  this.showRivalryByRaces (this.human,this.human, this.scoreHH1,this.scoreHH2 )
+  this.showRivalryByRaces (this.human,this.orc, this.scoreHO1,this.scoreHO2)
+  this.showRivalryByRaces (this.human,this.elf, this.scoreHE1,this.scoreHE2)
+  this.showRivalryByRaces (this.human,this.undead, this.scoreHU1,this.scoreHU2)
+  this.showRivalryByRaces (this.human,this.rnd, this.scoreHR1,this.scoreHR2)
+  this.showRivalryByRaces (this.elf,this.human, this.scoreEH1,this.scoreEH2)
+  this.showRivalryByRaces (this.elf,this.orc,this.scoreEO1,this.scoreEO2)
+  this.showRivalryByRaces (this.elf,this.elf, this.scoreEE1,this.scoreEE2)
+  this.showRivalryByRaces (this.elf,this.undead, this.scoreEU1,this.scoreEU2)
+  this.showRivalryByRaces (this.elf,this.rnd, this.scoreER1,this.scoreER2)
+  this.showRivalryByRaces (this.undead,this.human, this.scoreUH1, this.scoreUH2)
+  this.showRivalryByRaces (this.undead,this.orc, this.scoreUO1, this.scoreUO2)
+  this.showRivalryByRaces (this.undead,this.elf, this.scoreUE1, this.scoreUE2)
+  this.showRivalryByRaces (this.undead,this.undead, this.scoreUU1, this.scoreUU2)
+  this.showRivalryByRaces (this.undead,this.rnd, this.scoreUR1, this.scoreUR2)
+  this.showRivalryByRaces (this.rnd,this.human, this.scoreRH1, this.scoreRH2)
+  this.showRivalryByRaces (this.rnd,this.orc, this.scoreRO1, this.scoreRO2)
+  this.showRivalryByRaces (this.rnd,this.elf, this.scoreRE1, this.scoreRE2)
+  this.showRivalryByRaces (this.rnd,this.undead, this.scoreRU1, this.scoreRU2)
+  this.showRivalryByRaces (this.rnd,this.rnd, this.scoreRR1, this.scoreRR2)
+/* console.log(this.scoreOH1,this.scoreOH2,this.scoreOO1,this.scoreOO2,this.scoreOE1,this.scoreOE2,this.scoreOU1,this.scoreOU2,this.scoreOR1,this.scoreOR2,this.scoreUR1, this.scoreUR2,this.scoreUU1, this.scoreUU2,this.scoreUE1, this.scoreUE2, this.scoreUO1, this.scoreUO2,this.scoreUH1, this.scoreUH2 ) */
+ 
+  this.raceArray=[];
 }
 
 
